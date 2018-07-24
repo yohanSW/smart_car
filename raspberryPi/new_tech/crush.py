06acb439bfe049860830138f7635e4ea9e097b2e
foreverka2 <39242487+foreverka2@users.noreply.github.com>
2018-07-24 11:34:37 +0900

    Update crush.py

--- raspberryPi/new_tech/crush.py
+++ raspberryPi/new_tech/crush.py
@@ -1,3 +1,9 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+pin = 11
+
 def crush():
     #set var
 
@@ -15,14 +21,21 @@ def crush():
                 #if heart attack has been happend, Auto driving must be started!!
                 Accident = 1
 
-            if impact == 1 and sona <= 40 :
+            if impact == 1 and ( sona <= 40 or sona >= 1000 ) :
                 print("Car crush has been happened")
                 Accident = 1
         
         
-            if Accident == 1 :
-                gpsx, gpsy = gps()
+            if Accident == 1 : # if Accident happend,
 
+                gpsx, gpsy = gps() # GPS signal is transmitted,
+
+                for i in range(5) : # led blinked 5 times.
+                    GPIO.output(pin,True)
+                    time.sleep(1)
+                    GPIO.output(pin,False)
+                    time.sleep(1)
+                GPIO.cleanup()
                 break
 
 
