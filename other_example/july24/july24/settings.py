7def03430edd4f81e584593ed472c223cbd9096b
choonghoney <choonghwan1215@gmail.com>
2018-07-24 23:48:39 +0900

    revise DIRS
    revise DIRS

--- other_example/july24/july24/settings.py
+++ other_example/july24/july24/settings.py
@@ -110,7 +110,7 @@ ROOT_URLCONF = 'july24.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
