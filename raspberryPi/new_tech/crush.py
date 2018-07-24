55c74efe3c3890156661f55126097e9b62991855
foreverka2 <39242487+foreverka2@users.noreply.github.com>
2018-07-24 11:19:43 +0900

    Update crush.py

--- raspberryPi/new_tech/crush.py
+++ raspberryPi/new_tech/crush.py
@@ -1,1 +1,29 @@
-ddd
+def crush():
+    #set var
+
+    while True :
+        while True :
+
+            Accident = 0 
+
+            if fire == 1 :
+                print("Fire Fire Fire")
+                Accident = 1
+
+            if heartpulse >= 130 and heartpulse <=40 :
+                print("Driver is under heart attack")
+                #if heart attack has been happend, Auto driving must be started!!
+                Accident = 1
+
+            if impact == 1 and sona <= 40 :
+                print("Car crush has been happened")
+                Accident = 1
+        
+        
+            if Accident == 1 :
+                gpsx, gpsy = gps()
+
+                break
+
+
+def sensing():
