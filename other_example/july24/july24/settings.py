c70409d69d1c1397b49c6af5d161c6529baba3e6
choonghoney <choonghwan1215@gmail.com>
2018-07-25 11:06:24 +0900

    test
    test

--- other_example/july24/july24/settings.py
+++ other_example/july24/july24/settings.py
@@ -119,11 +119,11 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.contrib.auth.context_processors.auth',
-                'django.core.context_processors.debug',
-                'django.core.context_processors.i18n',
-                'django.core.context_processors.media',
-                'django.core.context_processors.static',
-                'django.core.context_processors.request',
+                #'django.core.context_processors.debug',
+                #'django.core.context_processors.i18n',
+                #'django.core.context_processors.media',
+                #'django.core.context_processors.static',
+                #'django.core.context_processors.request',
                 'django_facebook.context_processors.facebook',
 
                 'social_django.context_processors.backends',  # new append  
