0e78cbb5ce04175b716e0132368b8d836a6ae2dc
kdi321 <emailkey@hanmail.net>
2018-08-08 11:48:48 +0900

    Revert "3조 자율주행코드- 신기술 연계 코드 정리"
    This reverts commit 5adf95f6e77316362d81dd169b1761aa07ca461d.

--- arduino/Motor_Control/Motor_Control.ino
+++ arduino/Motor_Control/Motor_Control.ino
@@ -5,22 +5,22 @@
 #define swpin 12 //엔코더, 스위치핀
 
 /* 조향 모터 부분 */
-#define mt_STOP 8 // 모터드라이버 -> 릴레이, LOW: 모터 작동 / HIGH: 모터 정지 
-#define mt_DIR 7 // 모터드라이버 -> 릴레이, LOW: CW / HIGH: CCW
-#define mt_SPEED 5 // 모터드라이버, PWM을 통한 모터 속도제어
+#define BRAKE 8 // 모터드라이버 -> 릴레이, LOW: Brake 해제 / HIGH: Brake
+#define DIR 7 // 모터드라이버 -> 릴레이, LOW: CW / HIGH: CCW
+#define SPEED 5 // 모터드라이버, PWM을 통한 모터 속도제어
 #define gear_ratio 0.162 // 스티어링과 엔코더의 기어비 (73/25)*(20/360) {3조}
 //#define gear_ratio 0.18 // 스티어링과 엔코더의 기어비 (108/25)*(15/360) {2조}
 
 /* 브레이크 모터 부분 */
 #define brk_DIR 13 // 모터드라이버 -> 릴레이, LOW: CW / HIGH: CCW
-#define brk_RELEASE 2 // 모터드라이버 -> 릴레이, HIGH: 브레이크 모터 정지 / LOW: 브레이크 모터 작동
+#define brk_BRAKE 2 // 모터드라이버 -> 릴레이, HIGH: brk_BRAKE 해제 / LOW: brk_BRAKE
 #define brk_SPEED 5 // 모터드라이버, PWM을 통한 모터 속도제어
 
 /* 자율주행여부, 브레이크제어 코드 */
-#define auto_DRI 9 // 자율주행 여부를 아두이노 신호로 받음  (1 : 자율주행 0 : 수동주행)
-#define auto_STOP 10  //2조 
+#define auto_DRI 9
+#define auto_STOP 10
 int is_driving = 0;
-int brake_order = 0;
+int break_order = 0;
 int encoder_boundary = 84; // {3조}
 //int encoder_boundary = 102; // {2조}
 
@@ -37,24 +37,24 @@ static int oldB = HIGH;
 /* Serial 통신 초기화 */
 long wheel_angle=0; // 조향각 (바퀴의 각도)
 int minus_sig=1; // 조향각 (바퀴의 각도)
-int is_brake=0; // 차량 운행 정지 여부, 0: 운행 / 1: 정지
-int is_brakeING = 0; // 브레이크 모터가 동작 중인가? 0: 브레이크모터 동작 X / 1: 브레이크모터 동작 O
+int is_break=0; // 차량 운행 정지 여부, 0: 운행 / 1: 정지
+int is_breakING = 0; // 브레이크 모터가 동작 중인가? 0: 브레이크모터 동작 X / 1: 브레이크모터 동작 O
 
 void setup() {
  pinMode (clpin, INPUT);
  pinMode (dtpin, INPUT);
  pinMode (swpin, INPUT);
- pinMode (mt_STOP, OUTPUT);
- pinMode (mt_SPEED, OUTPUT);
- pinMode (mt_DIR, OUTPUT);
- pinMode (brk_RELEASE, OUTPUT);
+ pinMode (BRAKE, OUTPUT);
+ pinMode (SPEED, OUTPUT);
+ pinMode (DIR, OUTPUT);
+ pinMode (brk_BRAKE, OUTPUT);
  pinMode (brk_SPEED, OUTPUT);
  pinMode (brk_DIR, OUTPUT);
  pinMode (auto_DRI, INPUT);
  pinMode (auto_STOP, INPUT); 
- digitalWrite (brk_RELEASE , LOW); // 데이터를 입력받기 전엔 브레이크 모터는 정지상태로 시작
+ digitalWrite (brk_BRAKE , LOW); // 데이터를 입력받기 전엔 브레이크 모터는 정지상태로 시작
  digitalWrite (swpin, HIGH); // encoder 동작을 위한 switch ON
- digitalWrite (mt_STOP, HIGH); // 데이터를 입력받기 전엔 모터는 정지상태로 시작
+ digitalWrite (BRAKE, HIGH); // 데이터를 입력받기 전엔 모터는 정지상태로 시작
  Serial.begin(9600);
 }
 
@@ -67,15 +67,13 @@ void loop() {
   steer_angle = wheel_angle * 13 ; // 스티어링 각도와 조향 각도의 비 13 : 1
   degree = steer_angle * gear_ratio;
 
-  //자율주행 모드 
   if(is_driving != 0){
-    brake_mode(); // 브레이크 작동여부 체크
-    control(degree); // 조향 작동
+    break_mode();
+    control(degree);
   }
-  //수동주행 모드
   else{
-    encoderVal = encoderVal + getEncoderTurn(); // encoder 각도 갱신, encoder 값이 틀어지는 것을 방지하는것
-    digitalWrite(mt_STOP,HIGH); // 조향모터 정지 
+    encoderVal = encoderVal + getEncoderTurn(); // encoder 각도 갱신
+    digitalWrite(BRAKE,HIGH);
   }
     
 } // loop문 괄호
@@ -86,11 +84,9 @@ void get_data(){
   while(true) 
   {
     
-    is_driving = digitalRead(auto_DRI);  // 선택사항 -> 자율주행 모드 연결시 사용(아두이노에서 넘어오는 신호, 모터 작동 여부 결정)
-    //is_driving = 1;      
-    /*2조        
-    brake_order = digitalRead(auto_STOP);
-    */
+    //is_driving = digitalRead(auto_DRI);  // 선택사항 -> 자율주행 모드 연결시 사용
+    is_driving = 1;              
+    break_order = digitalRead(auto_STOP);
     
     if(Serial.available())
     {
@@ -100,11 +96,9 @@ void get_data(){
         wheel_angle=Serial.parseInt();
         if(minus_sig == 0)
           wheel_angle = -wheel_angle;
-        /* 2조
-        is_brake = Serial.parseInt();
-        if(is_brake == 1 || brake_order ==1)
-          is_brake = 1;
-        */
+        is_break = Serial.parseInt();
+        if(is_break == 1 || break_order ==1)
+          is_break = 1;
         Serial.print("wheel_angle : ");
         Serial.println(wheel_angle); //0.5초 딜레이 동안 받는 신호 수 만큼 angle 출력
         
@@ -118,38 +112,28 @@ void get_data(){
   }
 }
 
-void brake_mode(){
-  // 브레이크 잠긴 상태로 신호가 꺼졌을 경우 대비용
-    if (is_break == 2)
-  {
-    digitalWrite(brk_DIR,HIGH); // 당겨진 브레이크 모터 풀어주기 위해 방향 반대로 설정
-    digitalWrite(brk_RELEASE,LOW); // 고정된 브레이크 모터 해제
-    digitalWrite(brk_SPEED,255);
-    delay(700); 브레이크 모터 해제
-    digitalWrite(brk_RELEASE,HIGH); // 해체 한 상태로 브레이크 모터 고정
-    is_brakeING = 0; //브레이크모터는 더 이상 동작하지 않으므로, 다음 정지동작을 위해 0으로 초기화
-  }
-  if(is_brake == 1 && is_brakeING == 0) //정지 신호 발생, 그리고 원래 브레이크모터 동작은 없었음
+void break_mode(){
+  if(is_break == 1 && is_breakING == 0) //정지 신호 발생, 그리고 원래 브레이크모터 동작은 없었음
   { 
     digitalWrite(brk_DIR,LOW); // 항상 CW방향으로 회전
-    digitalWrite(brk_RELEASE,LOW); //정지 동작을 위해 브레이크 모터 고정 해제   
+    digitalWrite(brk_BRAKE,HIGH); //정지 동작을 위해 브레이크 모터 고정 해제   
     digitalWrite(brk_SPEED,255);   
-    delay(700); // 1.5초 동안 브레이크 모터를 동작시켜 정지 동작 수행   
-    digitalWrite(brk_RELEASE,HIGH); // 차량 브레이크가 당겨진 상태로 고정
-    is_brakeING = 1; //브레이크모터가 동작중인데 loop를 돌아 중복하여 브레이크모터 동작 방지
+    delay(1500); // 1.5초 동안 브레이크 모터를 동작시켜 정지 동작 수행   
+    digitalWrite(brk_BRAKE,LOW); // 차량 브레이크가 당겨진 상태로 고정
+    is_breakING = 1; //브레이크모터가 동작중인데 loop를 돌아 중복하여 브레이크모터 동작 방지
   }
-  else if(is_brake ==1 && is_brakeING == 1) // 여전히 정지 신호 발생, 브레이크 모터는 동작 중
+  else if(is_break ==1 && is_breakING == 1) // 여전히 정지 신호 발생, 브레이크 모터는 동작 중
   {
     delay(500);
   }
-  else if(is_brake == 0 && is_brakeING == 1) // 브레이크 밟힌 상태, 정지 신호 해제
+  else if(is_break == 0 && is_breakING == 1) // 브레이크 모터 동작중, 정지 신호 해제
   {
     digitalWrite(brk_DIR,HIGH); // 당겨진 브레이크 모터 풀어주기 위해 방향 반대로 설정
-    digitalWrite(brk_RELEASE,LOW); // 고정된 브레이크 모터 해제
+    digitalWrite(brk_BRAKE,HIGH); // 고정된 브레이크 모터 해제
     digitalWrite(brk_SPEED,255);
-    delay(700); 브레이크 모터 해제
-    digitalWrite(brk_RELEASE,HIGH); // 해체 한 상태로 브레이크 모터 고정
-    is_brakeING = 0; //브레이크모터는 더 이상 동작하지 않으므로, 다음 정지동작을 위해 0으로 초기화
+    delay(1500); // 1.5초 동안 브레이크 모터 해제
+    digitalWrite(brk_BRAKE,LOW); // 해체 한 상태로 브레이크 모터 고정
+    is_breakING = 0; //브레이크모터는 더 이상 동작하지 않으므로, 다음 정지동작을 위해 0으로 초기화
   }
   else ;// 정시 신호 없음, 그리고 브레이크 동작도 없었음. 즉 정상주행
 }
@@ -188,7 +172,7 @@ void control(int degree){
 
     Serial.print("encoderVal : ");  
     Serial.println(encoderVal);
-    digitalWrite(mt_STOP,LOW); // 모터동작을 시작하기 위해 브레이크 해제
+    digitalWrite(BRAKE,LOW); // 모터동작을 시작하기 위해 브레이크 해제
     if (encoderVal <= degree) // 방향 제어
       digitalWrite(DIR, LOW);// CW 방향
     else 
@@ -198,12 +182,12 @@ void control(int degree){
     /* 실제 엔코더의 각도가 원하는 엔코더 각도 범주 안에 들어올 경우 */
     /* 브레이크를 작동하고 while문을 벗어난다 */
     if ( encoderVal >= degree-2 && encoderVal <= degree+2 ){
-      digitalWrite(mt_STOP,HIGH);
+      digitalWrite(BRAKE,HIGH);
       mt_ctrl_cnt=0;
-      brake;
+      break;
     }
    else if(encoderVal >= encoder_boundary || encoderVal <= -encoder_boundary ){
-      digitalWrite(mt_STOP,HIGH);
+      digitalWrite(BRAKE,HIGH);
       if(encoderVal >= encoder_boundary){
         encoderVal = encoderVal - 1;
         mt_ctrl_cnt++;
@@ -212,7 +196,7 @@ void control(int degree){
         encoderVal = encoderVal + 1;
         mt_ctrl_cnt--;
       }
-      brake;
+      break;
     }
     
     if(mt_ctrl_cnt == 1){
--- arduino/new_Tech_3jo/clutch_Tech/Automatic_clutch/Automatic_clutch.ino
+++ arduino/new_Tech_3jo/clutch_Tech/Automatic_clutch/Automatic_clutch.ino
@@ -1,32 +1,31 @@
-#define clutch_pin 8 // automatic clutch pin -> 클러치 결착 신호
-#define signalToMotor 9 // 자율주행 모드 변환 후 조향모터 작동을 위한 신호 발생 -> 자율주행 할지 말지 
+#define clutch_pin 8 // automatic clutch pin
+#define signalToMotor 9 // 자율주행 모드 변환 후 조향모터 작동을 위한 신호 발생
 int signal_clutch = 0;
 
 void setup() {
-  pinMode(clutch_pin, OUTPUT); // 전자클러치로 연결되는 아웃풋
-  pinMode(signalToMotor, OUTPUT); // 라즈베리파이로 연결되는 아웃풋
+  pinMode(clutch_pin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   while(true){
     if(Serial.available()){
-      signal_clutch = Serial.parseInt(); // 보이스키트에서 자유주행관련 신호 전달 받음
+      signal_clutch = Serial.parseInt();
       Serial.print("FROM raspberry ");
       Serial.println(signal_clutch);
       if(signal_clutch ==1){
         Serial.println("Automatic clutch ON");
-        digitalWrite(clutch_pin, HIGH); // 전자클러치 연결
-        digitalWrite(signalToMotor, HIGH); // 메인 라즈베리에 신호 보냄
+        digitalWrite(clutch_pin, HIGH);
+        digitalWrite(signalToMotor, HIGH);
       }
       else{
         Serial.println("Automatic clutch OFF");
-        digitalWrite(clutch_pin, LOW); // 전자클러치 해제
+        digitalWrite(clutch_pin, LOW);
         digitalWrite(signalToMotor, LOW);
       }
     }
     else{
-        #digitalWrite(signalToMotor, LOW); // 아두이노는 기본으로 1값을 가지기 때문에 필요 없음 
+        digitalWrite(signalToMotor, LOW);
     }
   }
   
