42fac07c0b38a7e4cee3e5ce81fa6fc17070665a
Yohan Kim <38126890+yohanSW@users.noreply.github.com>
2018-08-08 16:32:57 +0900

    change image_cnt
    change image_cnt

--- raspberryPi/2jo/main.py
+++ raspberryPi/2jo/main.py
@@ -22,7 +22,7 @@ adjusted_angle      = 0    # Calibrate the front wheel angle whose direction is 
 approach_angle      = 3
 error_signal        = -700
 turning_max         = 40
-image_cnt			= 7
+image_cnt			= 3
 
 cw = controlWheels.ControlWheels()
 cw.offset = 0
