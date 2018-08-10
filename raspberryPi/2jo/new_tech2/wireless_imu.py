4dfa2670f2aca05cb315c2986f81d1f0a9d2f5c7
Yohan Kim <38126890+yohanSW@users.noreply.github.com>
2018-08-10 14:17:38 +0900

    write imu code
    write imu code

--- raspberryPi/2jo/new_tech2/wireless_imu.py
+++ raspberryPi/2jo/new_tech2/wireless_imu.py
@@ -1,6 +1,6 @@
 import socket, traceback
 
-host = '172.20.10.3'
+host = '192.168.0.113'
 port = 5555
 s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -8,17 +8,31 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.bind((host, port))
 
 def main():
+    message, address = s.recvfrom(8192)
+    #print(str(message))
+    x = float(str(message).split(',')[6])
+    y = float(str(message).split(',')[7])
+    z = float(str(message).split(',')[8])
+
     while True:
         try:
             message, address = s.recvfrom(8192)
-            print(message)
+            #print(str(message))
+            changeX
+            x = float(str(message).split(',')[6])
+            y = float(str(message).split(',')[7])
+            z = float(str(message).split(',')[8])
+            print(x,y,z)
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
             traceback.print_exc()
 
+def destroy():
+    return 0
+
 if __name__ == '__main__':
-try:
-main()
-except KeyboardInterrupt:
-destroy()
+    try:
+        main()
+    except KeyboardInterrupt:
+        destroy()
