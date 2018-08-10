05f9f6912d504f2c9450dee5ab53d906ea5233f0
Derrick5673 <chihun5673@daum.net>
2018-08-10 09:55:05 +0900

    Update image_processing.py

--- raspberryPi/2jo/image_processing.py
+++ raspberryPi/2jo/image_processing.py
@@ -21,8 +21,8 @@ import math
 import time
 #img = cv2.VideoCapture(0)
 #img = cv2.videoCapture('/home/pi/Downloads/lastYear.mp4')
-SCREEN_WIDTH = 1280  # Screen Width
-SCREEN_HEIGHT = 720  # Screen Height
+SCREEN_WIDTH = 1024  # Screen Width
+SCREEN_HEIGHT = 768  # Screen Height
 
 def image_processing(img):
     # Load input image
