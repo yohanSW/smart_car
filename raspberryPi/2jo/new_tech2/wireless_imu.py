797e3fc052a236b068f328da644903a70bce867d
Yohan Kim <38126890+yohanSW@users.noreply.github.com>
2018-08-10 11:15:52 +0900

    imu main code
    imu main code

--- raspberryPi/2jo/new_tech2/wireless_imu.py
+++ raspberryPi/2jo/new_tech2/wireless_imu.py
@@ -6,11 +6,19 @@ s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.bind((host, port))
-while 1:
-   try:
-      message, address = s.recvfrom(8192)
-      print(message)
-   except (KeyboardInterrupt, SystemExit):
-      raise
-   except:
-      traceback.print_exc()
+
+def main():
+    while True:
+        try:
+            message, address = s.recvfrom(8192)
+            print(message)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            traceback.print_exc()
+
+if __name__ == '__main__':
+try:
+main()
+except KeyboardInterrupt:
+destroy()
